802a5f3378e6b59e0f1d023bcfdde908711cc4e9
axel tavares <tavaresaxel95@gmail.com>
2024-01-31 23:29:15 -0500

    added another API for 5 day forecast and creted for loop and inner HTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 var apikey = "86de7b2c36c7bf67600a2051e5b6e7b2"
+var dashboardEl = document.getElementById("dashboard")
 
 function currentWeather(cityName){
     var url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apikey}&units=imperial`
@@ -9,6 +10,14 @@ function currentWeather(cityName){
     })
     .then(function(data){
         console.log(data)
+
+        dashboardEl.innerHTML = `
+        <h3>${data.name} (${dayjs.unix(data.dt).format("MM/DD/YYYY")}) <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt=""></h3>
+        <p>Temp: 75 F</p>
+        <p>Wind: 8.43 MPH</p>
+        <p>Humidity: 44%</p>`
+
+        
     })
 
 }
@@ -20,9 +29,16 @@ function fiveDayForecast(cityName){
     .then(function(response){
         return response.json()
     })
-    .then(fuction(data){
+    .then(function(data){
         console.log(data)
+
+        for(var i = 3; i < data.list.length ; i = i + 8  ){
+            var fiveDayArray = data.list
+            console.log(fiveDayArray[i])
+        }
+        
     })
 }
 
 currentWeather('chicago')
+fiveDayForecast('Chicago')
